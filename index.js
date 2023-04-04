d117fb7d32434b8eb4b7e4bee60b77a3201368b8
sanjeetjha-hub <1999sanjeetjha@gmai.com>
2023-04-04 09:38:02 +0530

    Handling movie selection

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const fetchData = async (searchItem) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
+const fetchData = async (searchItem) => {                                       //fetching the data from server, using are using the search endpoint to serch movies
+    const response = await axios.get('http://www.omdbapi.com/', {                                                                       
         params: {
             apikey: '1f06565f',
             s: searchItem
         }
     });
-    if (response.data.Error) {
-        return [];
+    if (response.data.Error) {                                                //THIS is a edge case we have handled when there is error or no data present then the api return a string and       
+        return [];                                                            //we get error while iterating over the reponse below, we are returning empty array 
     }
     return response.data.Search;
 };
 
 const root = document.querySelector('.autoComplete');
+const searchButton1 = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
+
 root.innerHTML = `
     <label><b>Search for a Movie</b></label>
     <input class= "input"/>
@@ -23,9 +28,6 @@ root.innerHTML = `
     </div>
 `;
 
-const searchButton1 = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
 
 
 
@@ -33,6 +35,11 @@ const onInput = async (event) => {
 
     const movies = await fetchData(event.target.value);
 
+    if(!movies.length)
+    {
+        dropdown.classList.remove('is-active');
+        return;
+    }
     resultsWrapper.innerHTML = '';
 
     dropdown.classList.add('is-active');
@@ -48,8 +55,12 @@ const onInput = async (event) => {
             <img src="${imgSrc}"/>
             ${movie.Title}
             `;
+        
+        option.addEventListener('click', () => {
+            dropdown.classList.remove('is-active');
+            searchButton1.value = movie.Title;
+        });
         resultsWrapper.append(option);
-        console.log(resultsWrapper);
     }
 };
 
@@ -57,11 +68,9 @@ searchButton1.addEventListener('input', debounce(onInput, 500));
 
 document.addEventListener('click', (event) => {
     if (!root.contains(event.target)) {
-
         dropdown.classList.remove('is-active');
-        console.log(dropdown.classList);
     }
-    // if (!dropdown.classList.value.includes('is-active') && searchButton1.value != '') {
-    //     dropdown.classList.add('is-active');
-    // };
+    if (!dropdown.classList.value.includes('is-active') && searchButton1.value != '' && searchButton1.contains(event.target)) {
+        dropdown.classList.add('is-active');
+    };
 });
